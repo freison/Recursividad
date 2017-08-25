990d80066a9be630e196ffe9e62774033391b2fa
Freison Castro Sánchez <FreisonCastro@hotmail.com>
2017-08-25 14:03:22 -0600

    Starting

---
+++ Ejemplo/Ejemplo/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo
+{
+    public class Class1
+    {
+        int n;
+    }
+}
